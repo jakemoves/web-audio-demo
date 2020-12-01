53cccd2a179a742f40d85628b0eea839ae238a4e
Jacob Niedzwiecki <jake@jacob-n.com>
2020-12-01 15:26:23 -0700

    Added stop button

--- js/web-audio-session.js
+++ js/web-audio-session.js
@@ -11,7 +11,7 @@ X prepares for playback
 */
 
 var WebAudioSession = {
-  players: {}, // sample buffers to hold each track
+  players: new Map(), // sample buffers to hold each track
   fader: null, // crossfader
 
   enable: async function(audioAssets){
@@ -22,19 +22,20 @@ var WebAudioSession = {
     } // most errors here are the result of mobile browsers trying to prevent 'autoplaying' audio which is usually annoying to users
 
     audioAssets.forEach( (audioAsset, index) => {
-      this.players[index] = new Tone.Player(audioAsset);
+      this.players.set("" + index, new Tone.Player(audioAsset))
     })
 
+    console.log(this.players)
     console.log('audio session started')
 
     Tone.loaded().then(() => {
       console.log('loading complete')
       // setting up crossfade
-      if(Object.keys(this.players).length == 2){ // make sure there's only two samples / tracks
+      if(this.players.size == 2){ // make sure there's only two samples / tracks
         this.fader = new Tone.CrossFade().toDestination()
         // connect the two inputs
-        const ambient = this.players["0"].connect(this.fader.a).start()
-        const cue = this.players["1"].connect(this.fader.b).start()
+        const ambient = this.players.get("0").connect(this.fader.a).start()
+        const cue = this.players.get("1").connect(this.fader.b).start()
         this.fader.fade.value = 0.5
       } else {
         console.log("Error -- this patch setup needs exactly two files passed in")
@@ -46,11 +47,12 @@ var WebAudioSession = {
     this.fader.fade.value = value // set the value of the crossfader to the user-set value of the slider
   },
 
-  // pauseAll: function(){
-  //   this.players.forEach( player => {
-  //     player.pause()
-  //   })
-  // }
+  stopAll: function(){
+    this.players.forEach( player => {
+      console.log(player)
+      player.stop()
+    })
+  }
 }
 
 export { WebAudioSession }
