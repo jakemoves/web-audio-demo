45e73beb76440906ad50c2f2e7453e03233b1031
Cultch-DS <74624977+Cultch-DS@users.noreply.github.com>
2020-12-04 21:42:51 -0800

    updated all episode-labels

--- js/index.js
+++ js/index.js
@@ -48,21 +48,21 @@ let episodes = [
     ]
   },{
     episodeNumber: 2,
-    episodeLabel: "Voyage",
+    episodeLabel: "Part 2: The Voyage",
     audioAssets:[
       "./sounds/HC-Winter-Amb-1Test-short.mp3",
       "./sounds/Wonderland_Window-Place.mp3"
     ]
   },{
     episodeNumber: 3,
-    episodeLabel: "Place",
+    episodeLabel: "Part 3: The Meeting Place",
     audioAssets:[
       "./sounds/HC-Winter-Amb-1Test-short.mp3",
       "./sounds/Wonderland-Scene3-2.5MThePlace.mp3"
     ]
   },{
     episodeNumber: 4,
-    episodeLabel: "Spring",
+    episodeLabel: "Part 4: Spring",
     audioAssets:[
       "./sounds/HC-Winter-Amb-1Test-short.mp3",
       "./sounds/Wonderland-Scene4-2M-Spring.mp3"
